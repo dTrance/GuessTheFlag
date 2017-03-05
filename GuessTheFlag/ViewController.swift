9d199550db233e089447f6f13ba2c8ea366c0a56
Mark Trance <trance@Marks-iMac.local>
2017-03-05 14:41:09 -0500

    Initial Commit

---
+++ GuessTheFlag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Mark Trance on 3/5/17.
+//  Copyright © 2017 Mark Trance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
